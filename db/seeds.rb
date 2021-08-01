56ebec6936820a2ceda5ab85fa03bb9c662ac6d7
kwansai724 <kwansai.taka724@gmail.com>
2021-08-02 08:57:14 +0900

    sedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  name = "サンプルユーザー#{n+1}"
+  email = "sample#{n+1}@email.com"
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    confirmed_at: Time.now
+  )
+end
+puts "サンプルユーザー作成"
+
+
+Plan.create!(
+  name: "膣美人オンラインサロン",
+  price: "3,000",
+  plan_id: "1"
+)
+puts "プラン作成"
