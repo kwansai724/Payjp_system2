f30d120495b20110ef2f5d5e1e596174c0fcfdc6
kwansai724 <kwansai.taka724@gmail.com>
2021-08-02 20:06:59 +0900

    seedファイルを本番と開発に分ける

---
+++ db/seed/production.rb
@@ -0,0 +1,6 @@
+Plan.create!(
+  name: "膣美人オンラインサロン",
+  price: "3,000",
+  plan_id: "1"
+)
+puts "プラン作成"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do |n|
-  name = "サンプルユーザー#{n+1}"
-  email = "sample#{n+1}@email.com"
-  password = "password"
-  User.create!(
-    name: name,
-    email: email,
-    password: password,
-    password_confirmation: password,
-    confirmed_at: Time.now
-  )
-end
-puts "サンプルユーザー作成"
+load(Rails.root.join("db", "seed", "#{Rails.env.downcase}.rb"))
 
+# 開発環境
+# ⇒rails db:seed
 
-Plan.create!(
-  name: "膣美人オンラインサロン",
-  price: "3,000",
-  plan_id: "1"
-)
-puts "プラン作成"
+# 本番環境
+# ⇒rails db:seed RAILS_ENV=production
