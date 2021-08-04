c483dd8009b8fd3abe738476bebf9c59e14ecef4
kwansai724 <kwansai.taka724@gmail.com>
2021-08-04 23:24:11 +0900

    production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,21 +68,35 @@ Rails.application.configure do
   # config.action_mailer.perform_deliveries = true
 
   
+  # host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
+  # config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.raise_delivery_errors = true
+
+  # config.action_mailer.perform_caching = false
+
+  # config.action_mailer.smtp_settings = {
+  #   address:"smtp.gmail.com",
+  #   domain: 'gmail.com',
+  #   port: 587,
+  #   user_name: ENV['YOUR_GMAIL_ADDRESS'],
+  #   password: ENV['GOOGLE_APP_PASSWORD'],
+  #   authentication: :login,
+  #   enable_starttls_auto: true
+  # }
+
+  # herokuでメールを送信するための設定
+  config.action_mailer.delivery_method = :smtp
   host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port: 587,
-    user_name: ENV['YOUR_GMAIL_ADDRESS'],
-    password: ENV['GOOGLE_APP_PASSWORD'],
-    authentication: :login,
-    enable_starttls_auto: true
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['YOUR_GMAIL_ADDRESS'],
+    :password       => ENV['GOOGLE_APP_PASSWORD'],
+    :domain         => 'gmail.com',
+    :enable_starttls_auto => true
   }
 
 
