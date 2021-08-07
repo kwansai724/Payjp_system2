30faf059be9ed1865a12a4b41b243d530c8633bd
kwansai724 <kwansai.taka724@gmail.com>
2021-08-08 01:20:22 +0900

    顧客詳細画面作成

--- app/controllers/admins/payjp_controller.rb
+++ app/controllers/admins/payjp_controller.rb
@@ -8,5 +8,12 @@ class Admins::PayjpController < Admins::Base
   end
 
   def user_show
+    if params[:format].present?
+      @user = Payjp::Customer.retrieve(params[:format])
+      @subscription = Subscription.find(params[:user_id])
+      @plan = Plan.find(@subscription.plan_id)
+   else
+      @user = User.find(params[:user_id])
+    end
   end
 end
