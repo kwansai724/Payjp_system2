1561f850d5c45b4b8d7badbbb6359109d561c678
kwansai724 <kwansai.taka724@gmail.com>
2021-08-16 23:11:07 +0900

    Subscriptionのモデルスペック作成（保留中）

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
     sequence(:plan_id) { |n| n }
     sequence(:name) { |n| "TestPlan#{n}" }
     price "3,000"
+
+    after(:create) do |plan|
+      create_list(:subscription, 3, plan: plan)
+    end
   end
 end
---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  it "プランはサブスクを複数持つことが出来ること" 
+end
