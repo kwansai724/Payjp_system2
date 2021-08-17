4e3bfa818814e02518310853e59f71bff7221237
kwansai724 <kwansai.taka724@gmail.com>
2021-08-17 19:56:55 +0900

    各画面の表示条件修正など

--- app/controllers/admins/payjp_controller.rb
+++ app/controllers/admins/payjp_controller.rb
@@ -4,12 +4,13 @@ class Admins::PayjpController < Admins::Base
   end
 
   def user_index
+    users = Payjp::Customer.all(limit: 3, offset: 10)
     @search_params = user_search_params
     @users = User.search(@search_params).page(params[:page]).per(10)
   end
 
   def user_show
-    if params[:format].present?
+    if Subscription.find_by(user_id: params[:user_id]).present?
       @user = Payjp::Customer.retrieve(params[:format])
       @subscription = Subscription.find_by(user_id: params[:user_id])
       @plan = Plan.find(@subscription.plan_id)
