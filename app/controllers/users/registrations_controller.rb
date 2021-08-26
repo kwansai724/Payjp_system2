e346d6b4aa4dc20be81882a90b57752e58a6d558
kwansai724 <kwansai.taka724@gmail.com>
2021-08-26 23:52:14 +0900

    アカウント登録後の遷移先設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -70,7 +70,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    new_user_session_path(resource)
-  end
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 end
